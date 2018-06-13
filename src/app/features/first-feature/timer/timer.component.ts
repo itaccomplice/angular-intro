7bfe4e8f23dffa7fc1d98e50b6118367215f2e5e
Mark Ford <mark.ford@agilesphere.co.uk>
2018-06-13 12:06:28 +0100

    exercise 2

--- src/app/features/first-feature/timer/timer.component.ts
+++ src/app/features/first-feature/timer/timer.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
-  constructor() { }
+  minutes: number;
+  seconds: number;
+
+  constructor(){
+    this.minutes = 24;
+    this.seconds = 59;
+    setInterval(() => this.tick(), 1000);
+  }
 
   ngOnInit() {
   }
 
+  tick() {
+    if(--this.seconds < 0) {
+      this.seconds = 59;
+      if(--this.minutes < 0) {
+        this.minutes = 24;
+        this.seconds = 59;
+      }
+    }
+  }
 }
