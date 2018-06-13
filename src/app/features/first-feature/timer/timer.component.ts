bdf5be48c7abe3123549045c6cc07756fb7498cc
Mark Ford <mark.ford@agilesphere.co.uk>
2018-06-13 12:59:21 +0100

    exercise3

--- src/app/features/first-feature/timer/timer.component.ts
+++ src/app/features/first-feature/timer/timer.component.ts
@@ -9,23 +9,41 @@ export class TimerComponent implements OnInit {
 
   minutes: number;
   seconds: number;
-
+  isPaused: boolean;
+  buttonLabel: string;
+  
   constructor(){
-    this.minutes = 24;
-    this.seconds = 59;
+    this.reset();
     setInterval(() => this.tick(), 1000);
   }
 
   ngOnInit() {
   }
 
-  tick() {
-    if(--this.seconds < 0) {
-      this.seconds = 59;
-      if(--this.minutes < 0) {
-        this.minutes = 24;
+  private tick() {
+    if(!this.isPaused) {
+      this.buttonLabel = 'Pause';
+      if(--this.seconds < 0) {
         this.seconds = 59;
+        if(--this.minutes < 0) {
+          this.reset();
+        }
       }
     }
+  }
+
+  togglePause() {
+    this.isPaused = !this.isPaused;
+    // if countdown has started
+    if (this.minutes < 24 || this.seconds < 59) {
+      this.buttonLabel = this.isPaused ? 'Resume' : 'Pause';
+    }
+  }
+
+  reset() {
+    this.minutes = 24;
+    this.seconds = 59;
+    this.buttonLabel = 'Start';
+    this.togglePause();
   }
 }
